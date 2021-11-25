e4bbfc86fecf82144d66be4b1d4a50004000abdc
bourne-3 <595962708@qq.com>
2021-11-25 14:51:03 +0800

    practice

--- TutorialPrac/prac1.go
+++ TutorialPrac/prac1.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// naked return
+func spilit(sum int) (x int, y int) {
+	x = sum + 1
+	y = sum - 1
+	return // naked
+}
+
+func twoSum(x int, y int) int {
+	return x + y
+}
+
 func main() {
-	fmt.Print("ok")
+	fmt.Println("ok")
+	fmt.Println(twoSum(1, 2))
+	fmt.Println(spilit(10))
 }
