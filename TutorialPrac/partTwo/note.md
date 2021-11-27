76871c5fc0cd483ee959c1a4814310d03a987778
bourne-3 <595962708@qq.com>
2021-11-27 11:07:39 +0800

    practice

--- TutorialPrac/partTwo/note.md
+++ TutorialPrac/partTwo/note.md
@@ -8,5 +8,13 @@
   - The first is so that the method can modify the value that its receiver points to.
   - The second is to avoid copying the value on each method call. 
 - 是用方法的时候会自动适应参数（方法调用）
-  - As a convenience, Go interprets the statement v.Scale(5) as (&v).Scale(5) since the Scale method has a pointer receiver.
-- 
+  - As a convenience, Go interprets the statement v.Scale(5) as (&v).Scale(5) since the Scale method has a pointer receiver
+
+### part2
+- 在内部，接口值可以看做包含值和具体类型的元组
+  - (value, type)
+  - 接口值保存了一个具体底层类型的具体值。 
+  - 接口值调用方法时会执行其底层类型的同名方法。
+- 未初始化的两个情况
+  - Interface values with nil underlying values
+  - Nil interface values （这个会报错）
--- TutorialPrac/partTwo/prac2.go
+++ TutorialPrac/partTwo/prac2.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+
+
+
+
+//==
 type I interface {
 	M()
 }
@@ -11,7 +16,12 @@ type T struct {
 }
 
 // T去是实现I
-func (t T) M() {
+func (t *T) M() {
+	// 如果传进来的是初始化的
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
 	fmt.Println(t.member)
 }
 
@@ -35,6 +45,10 @@ func (v *Vertex2) Abs() float64 {
 	return float64(v.x + 5 + v.y)
 }
 
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
 func run2() {
 	var i Abser
 
@@ -51,6 +65,14 @@ func run2() {
 	// 第三个 使用T去实现I
 	var i2 I  // 这是一个接口
 	t := T{"小白"}  // 这是一个实现了该接口的方法
-	i2 = t  // 接口可以接受实现了该接口下方法的值
+	i2 = &t  // 接口可以接受实现了该接口下方法的值
+	describe(i2)
 	i2.M()  // 有点像多态的调用
+
+	// =======
+	// 第四个 接口接收了初始化的val
+	var t2 *T
+	i2 = t2
+	describe(i2)
+	i2.M()
 }
