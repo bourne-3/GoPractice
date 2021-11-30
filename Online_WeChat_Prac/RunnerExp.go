c6bae1a0b4c0013a71cd42e99242b9ca6cf09357
bourne-3 <595962708@qq.com>
2021-11-30 15:48:19 +0800

    practice

--- Online_WeChat_Prac/RunnerExp.go
+++ Online_WeChat_Prac/RunnerExp.go
@@ -1,3 +1,23 @@
 package main
 
+import (
+	"os"
+	"time"
+)
+
+type Runner struct {
+	tasks []func(int)
+	complete chan error
+	timeout <-chan time.Time // 只能接收的单向通道
+	interrupt chan os.Signal
+}
+
+// 工厂
+func New(tm time.Duration) *Runner {
+	return &Runner{
+		complete: make(chan error),
+		timeout: time.After(tm),
+		interrupt: make(chan os.Signal, 1),  // 缓冲通道
+	}
+}
 
