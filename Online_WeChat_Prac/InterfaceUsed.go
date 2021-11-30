8cd49a0d5c3d3264babd2189f5944f3d393e78e9
bourne-3 <595962708@qq.com>
2021-11-30 10:50:37 +0800

    practice

--- Online_WeChat_Prac/InterfaceUsed.go
+++ Online_WeChat_Prac/InterfaceUsed.go
@@ -22,7 +22,19 @@ func run6() {
 	a = Dog(2)
 	a.printInfo()
 
-	//
+	// receiver是用值还是用指针的
+	var c2 = Cat(2)
+	invoke(c2)
+	invoke(&c2)  // 两者都可以传递给animal接口
+
+	var d1 = Duck(1)
+	invoke(&d1)
+	//invoke(d1)  // 这样就不行了，必须使用%d1传递
+
+}
+
+func invoke(a animal) {
+	a.printInfo()
 }
 
 type animal interface {
@@ -30,6 +42,12 @@ type animal interface {
 }
 type Cat int
 type Dog int
+type Duck int
+
+// 使用值接收者实现接口
+func (d *Duck) printInfo() {
+	fmt.Println("This is a duck. Implement by point receiver")
+}
 
 // 是方法实现接口
 func (c Cat) printInfo() {
